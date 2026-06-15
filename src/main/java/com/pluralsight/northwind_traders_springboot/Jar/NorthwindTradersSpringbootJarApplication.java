99e5cfadda25b457d86e04cb53622c58d0ee5489
Gabe <Carrollgbusiness@gmail.com>
2026-06-15 13:33:04 -0500

    first commit

---
+++ src/main/java/com/pluralsight/northwind_traders_springboot/Jar/NorthwindTradersSpringbootJarApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.northwind_traders_springboot.Jar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindTradersSpringbootJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindTradersSpringbootJarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluralsight/northwind_traders_springboot/Jar/NorthwindTradersSpringbootJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.northwind_traders_springboot.Jar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindTradersSpringbootJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
