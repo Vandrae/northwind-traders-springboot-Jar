6cb2744e40f7c49ad4d69ab4bf3baf39ae9ddb63
Gabe <Carrollgbusiness@gmail.com>
2026-06-16 15:09:16 -0500

    Add Getter/Setter to controller

--- src/main/java/com/pluralsight/northwind_traders_springboot/Model/Product.java
+++ src/main/java/com/pluralsight/northwind_traders_springboot/Model/Product.java
@@ -3,12 +3,53 @@ package com.pluralsight.northwind_traders_springboot.Model;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="Product")
+@Table(name="Products")
 public class Product {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ProductID")
     private int productId;
+
+    @Column(name = "ProductName")
     private String name;
+
+    @Column(name = "CategoryID")
     private String category;
+
+    @Column(name = "UnitPrice")
     private double price;
+
+    //Getter/setters
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
